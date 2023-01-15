b1b3b9a680bb4bb3827ebc23fd84cc3a3974127a
Preston Tom-Ying <preston.tomying@gmail.com>
2023-01-15 10:34:32 -0500

    dictionary big comments
    added rgb html element and javascript for HSL. Also finished dictionary for genres

--- src/Main.js
+++ src/Main.js
@@ -36,18 +36,6 @@ function openLink() {
   window.open("https://accounts.spotify.com/authorize?client_id=f3ec916c906f484c9ab9d3e8038aba05&response_type=code&redirect_uri=http://127.0.0.1:5500/Dyeify/src/index.html&scope=playlist-modify-private%20playlist-modify-public&state=some-state-of-my-choice");
 }
 
-var getArtist = function (grenre){
-  $.ajax({
-      url: 'https://api.spotify.com/v1/Search/',   
-      success: function (response) {  
-          callback(response);
-      }
-  });
-};
-
-function x(){
-console.log(getArtist);
-}
 
 
 // https://accounts.spotify.com:443/authorize?client_id=5fe01282e44241328a84e7c5cc169165&response_type=code&redirect_uri=https://example.com/callback&scope=user-read-private%20user-read-email&state=some-state-of-my-choice
