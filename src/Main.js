fca7d68179be79752706ee5534a03bb0f74482c2
Hon139 <andrew.yang139@gmail.com>
2023-01-15 09:34:33 -0500

    res

--- src/Main.js
+++ src/Main.js
@@ -41,6 +41,15 @@ function test(){
   return
 }
 
+function getArtist(grenre){
+  http
+  .createServer(function(request, response) {
+    response.writeHead(200, { 'Search-Quary': grenre });
+    response.end();
+  })
+}
+
+
 // https://accounts.spotify.com:443/authorize?client_id=5fe01282e44241328a84e7c5cc169165&response_type=code&redirect_uri=https://example.com/callback&scope=user-read-private%20user-read-email&state=some-state-of-my-choice
 // open(authorizeURL);
 
